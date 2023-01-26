883657f71b385719f762c9a9f29e58b596a0a35d
Tushar Saini <369shar@gmail.com>
2023-01-26 16:53:57 -0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BeastieHunter.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeastieHunter.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
